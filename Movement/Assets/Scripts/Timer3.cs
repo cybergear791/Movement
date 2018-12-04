a5554146c29c37db80a50dd2187fae68349aa855
Nick F <30716635+cybergear791@users.noreply.github.com>
2018-12-04 14:07:26 -0800

    Nicks Edits

---
+++ Movement/Assets/Scripts/Timer3.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer3 : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Renderer rend = GetComponent<Renderer>();
+		StartCoroutine (TriggerTheLight (rend));
+
+	}
+	
+
+	IEnumerator TriggerTheLight(Renderer rend)
+	{
+		rend.material.color = Color.red;
+		yield return new WaitForSeconds(.05f);
+		rend.material.color = Color.blue;
+		yield return new WaitForSeconds(.05f);
+		rend.material.color = Color.green;
+		yield return new WaitForSeconds(.05f);
+		rend.material.color = Color.yellow;
+		StartCoroutine (TriggerTheLight (rend));
+
+	}
+}
